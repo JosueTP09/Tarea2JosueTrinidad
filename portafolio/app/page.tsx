bc4c02667cc6e62967731fd398247df5683f47a4
JosueTP09 <josuetp249@gmail.com>
2026-04-24 03:38:16 +0000

    parte proyectos elaborada, profe se me olvido ponerle en el comit de intereses si me podia ayudar para que no se viera feo como me quedo, lo que pasa es que le movi yo  y me quedo asi como lo dejde se lo pedi a la ia y me lo habia dejado peor

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./Components / Header/Header";
 import Inicio from "./Components / Inicio/Inicio";
 import Intereses from "./Components / Intereses/Intereses";
 import CV from "./Components / CV/CV";
+import Proyectos from "./Components / Proyectos/Proyectos";
 import Contacto from "./Components / Contacto/Contacto";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Inicio />
       <Intereses />
       <CV />
+      <Proyectos />
       <Contacto />
     </>
   );
